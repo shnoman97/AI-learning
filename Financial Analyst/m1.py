ef8cc039e5b104f18b54c2a9d5d209385a277b1d
noman fareed <nomanfareed681@gmail.com>
2024-07-30 23:44:36 +0500

    refactor code

--- Financial Analyst/m1.py
+++ Financial Analyst/m1.py
@@ -1,11 +1,12 @@
 import os
+import joblib
 import agentops
 from embedchain import App
 
 from crewai import Agent, Task, Crew
 from textwrap import dedent
 
-from crewai_tools import PDFSearchTool, SerperDevTool, ScrapeWebsiteTool, tool
+from crewai_tools import SerperDevTool, ScrapeWebsiteTool, tool
 from langchain_groq import ChatGroq
 from llama_parse import LlamaParse
 
@@ -23,47 +24,48 @@ GROQ_LLM = ChatGroq(
 def tip_section():
     return "If you do your BEST WORK, I'll give you a $10,000 commision!"
 
-# def load_or_parse_data():
-#     data_dir = "./data"
-#     data_file = os.path.join(data_dir, "new-parsed-data.md")
+def load_or_parse_data(file_name):
+    data_dir = "./data"
+    data_file = os.path.join(data_dir, f"{file_name}.pkl")  # Use .pkl extension for joblib file
 
-#     if not os.path.exists(data_dir):
-#         os.makedirs(data_dir)
+    if not os.path.exists(data_dir):
+        os.makedirs(data_dir)
 
-#     if os.path.exists(data_file):
-#         # Load the parsed data from the file
-#         parsed_data = joblib.load(data_file)
-#     else:
-#         # Perform the parsing step and store the result in llama_parse_documents
-#         parsing_instruction_uber10k = """The provided document is a quarterly report filed by ATTOCK CEMENT PAKISTAN LIMITED
-#         with the Securities and Exchange Commission Pakistan (SECP).
-#         This form provides detailed financial information about the company's performance for a specific quarter.
-#         It includes unaudited financial statements, management discussion and analysis, and other relevant disclosures required by the SECP.
-#         It contains many tables.
-#         Try to be precise while answering the questions"""
+    if os.path.exists(data_file):
+        # Load the parsed data from the file
+        parsed_data = joblib.load(data_file)
+    else:
+        # Perform the parsing step and store the result in llama_parse_documents
+        parsing_instruction_uber10k = """The provided document is a quarterly report filed by a company listed
+        in the Securities and Exchange Commission Pakistan (SECP).
+        This form provides detailed financial information about the company's performance for a specific quarter.
+        It includes unaudited financial statements, management discussion and analysis, and other relevant disclosures required by the SECP.
+        It contains many tables.
+        Try to be precise while answering the questions"""
         
-#         parser = LlamaParse(api_key="llx-6S0lXWfj4wZMjlk72KpK6MZEDXDICvGFxunss41q8yrcziI2",
-#                             result_type="markdown",
-#                             parsing_instruction=parsing_instruction_uber10k,
-#                             max_timeout=5000)
-#         llama_parse_documents = parser.load_data("./files/230965.pdf")
+        parser = LlamaParse(api_key="llx-6S0lXWfj4wZMjlk72KpK6MZEDXDICvGFxunss41q8yrcziI2",
+                            result_type="markdown",
+                            parsing_instruction=parsing_instruction_uber10k,
+                            max_timeout=5000)
+        llama_parse_documents = parser.load_data(f"./files/{file_name}.pdf")
 
-#         # Save the parsed data to a file
-#         print("Saving the parse results in .pkl format ..........")
-#         joblib.dump(llama_parse_documents, data_file)
+        # Save the parsed data to a file
+        print("Saving the parse results in .pkl format ..........")
+        joblib.dump(llama_parse_documents, data_file)
 
-#         # Set the parsed data to the variable
-#         parsed_data = llama_parse_documents
+        # Set the parsed data to the variable
+        parsed_data = llama_parse_documents
 
-#     return parsed_data
+    return parsed_data
 
 
-# llama_parse_documents = load_or_parse_data()
+def store_file(file_name):
+    llama_parse_documents = load_or_parse_data(file_name)
 
-# print("llama_parse_documents : ", llama_parse_documents)
-# with open('data/output.md', 'a') as f:  # Open the file in append mode ('a')
-#         for doc in llama_parse_documents:
-#             f.write(doc.text + '\n')
+    print("llama_parse_documents : ", llama_parse_documents)
+    with open(f'data/{file_name}.md', 'a') as f:  # Open the file in append mode ('a')
+            for doc in llama_parse_documents:
+                f.write(doc.text + '\n')
 
 config = {
   'llm': {
@@ -82,38 +84,16 @@ config = {
 }
 
 app = App.from_config(config=config)
-app.add("./data/output.md")
+app.add("./data/output-1.md")
+
+def add_file(file_name):
+    app.add(f"./data/{file_name}.md")
 
 
 @tool
 def search_from_doc(inp: str):
     """Use this function to search from markdown file of a company's quarterly report."""
     return app.query(inp)
-
-
-pdf_tool = PDFSearchTool(
-    pdf="./files/230965.pdf",
-    config=dict(
-        llm=dict(
-            provider="groq", # or google, openai, anthropic, llama2, ...
-            config=dict(
-                model="llama3-70b-8192",
-                temperature=0.5,
-                # top_p=1,
-                # stream=true,
-            ),
-        ),
-        embedder=dict(
-            provider="ollama", # or openai, ollama, ...
-            config=dict(
-                model="all-minilm",
-                # task_type="retrieval_document",
-                # title="Embeddings",
-            ),
-        ),
-    )
-)
-
 
 
 financial_analyst = Agent(
@@ -126,10 +106,13 @@ financial_analyst = Agent(
       llm = GROQ_LLM,
       allow_delegation = False,
       tools=[search_from_doc, ScrapeWebsiteTool(), SerperDevTool()],
-      verbose=True
+      verbose=True,
+      max_rpm=12, # No limit on requests per minute
+      max_iter=12, # Default value for maximum iterations
     )
 
-financial_analysis = Task(
+def financial_analysis(output_file):
+    return Task(
     description=dedent(f"""
         Conduct a thorough analysis of the {{c_name}} financial
         health and market performance on the file. 
@@ -149,7 +132,7 @@ financial_analysis = Task(
         Make sure to use the most recent data as possible.
     """),
     agent=financial_analyst,
-    output_file='outputs/report.txt',
+    output_file=f'outputs/{output_file}-report.md',
     create_directory=True,
     expected_output="A detailed financial analysis report with assessments of the stock's financial standing, strengths and weaknesses, and comparisons with industry peers and market trends."
 )
@@ -160,12 +143,18 @@ crew = Crew(
         financial_analyst        
       ],
       tasks=[
-        financial_analysis,
+        financial_analysis("230965"),
       ],
       verbose=2,
-      cache=True
+      cache=True,
     )
 
-result = crew.kickoff(inputs={"c_name": "ACPL"})
-print(result)
+print("Storing File...")
+store_file("230965")  # Corrected file name
 
+print("File saved successfully")
+
+add_file("230965")
+
+result = crew.kickoff(inputs={"c_name": "ACPL", "file_name": "230965"})
+print(result)
